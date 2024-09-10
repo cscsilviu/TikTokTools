6676a565852817fbb698406fa1d1034e0280c0d0
Silviu CIMPU <silviu.cimpu@gmail.com>
2024-09-10 22:05:44 +0300

    test

--- TikTokTool/ConsoleApp1/Program.cs
+++ TikTokTool/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Va salut pe toti de pe TikTok");
+Console.WriteLine("Test");
